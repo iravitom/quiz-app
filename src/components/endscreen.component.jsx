671288beb0c288633e41a9c303af14cc545b2cc0
iravitom <iravitom@googlemail.com>
2022-03-17 22:23:50 +0530

    config

--- src/components/endscreen.component.jsx
+++ src/components/endscreen.component.jsx
@@ -1,19 +1,21 @@
 import { useContext } from "react";
 import { CurrentStateContext } from "../helpers/contexts";
 function EndComponent() {
-  const { score, userName, setCurrentState } = useContext(CurrentStateContext);
+  const { score, setScore, userName, setCurrentState } =
+    useContext(CurrentStateContext);
+  const reset = () => {
+    setCurrentState("quiz");
+    setScore(0);
+  };
   return (
     <div className="d-flex flex-column align-items-center">
       <h2 className="font">
         Hi <span className="username">{userName},</span> you have played well
         and your score is
-        <span className="score"> {score}</span>
+        <span className="score"> {score} / 30</span>
       </h2>
       <div className="mt-5">
-        <button
-          onClick={() => setCurrentState("quiz")}
-          className="btn btn-primary custom-btn mt-3"
-        >
+        <button onClick={reset} className="btn btn-primary custom-btn mt-3">
           Play Again
         </button>
       </div>
